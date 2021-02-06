ce9c37bcfeeec17ab6ae8124dd20c12f17c1202e
Steven <stevenvarela98@gmail.com>
2021-02-06 01:07:25 -0400

    removiendo item de carrito

--- src/screens/cart/cart.tsx
+++ src/screens/cart/cart.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card, CardHeader, Datatable } from 'components';
+import { Card, CardHeader, Datatable, Button } from 'components';
 import { Row, Col } from 'react-bootstrap';
-import {Product, DataTableColumn} from 'models';
+import {Product, Category, DataTableColumn} from 'models';
 import {RouteComponentProps} from 'react-router'
 import { connect, ConnectedProps } from "react-redux";
 import {RootState} from 'reducers'
@@ -22,8 +22,8 @@ class Cart extends React.Component<Props, State> {
         this.state = {
             columns: [
                 {
-                    name: '#',
-                    selector: 'id',
+                    name: 'Cantidad',
+                    selector: 'amount',
                     sortable: false,
                     center: true,
                 },
@@ -32,15 +32,71 @@ class Cart extends React.Component<Props, State> {
                     selector: 'name',
                     sortable: true,
                     center: true,
+                },
+                {
+                    name: 'Imagen',
+                    selector: 'img',
+                    sortable: false,
+                    center: true,
+                    cell: (element: Product) => <img
+                        className="text-center"
+                        style={{
+                            width: '60%'
+                        }}
+                        src={element.img}
+                        alt={element.name}
+                    />
+                },
+                {
+                    name: 'Precio',
+                    selector: 'price',
+                    sortable: false,
+                    center: true,
+                    cell: (element: Product) => <p className="text-center text-dark"> {element.price} </p>
+                },
+                {
+                    name: 'Precio',
+                    selector: 'price',
+                    sortable: false,
+                    center: true,
+                    cell: (element: Product) => <p className="text-dark w-100 text-center"> {
+                        element.categories.map((element: number | Category) => typeof element === 'number' ? '' : element.name)
+                            .join(' | ')
+                    } </p>
+                },
+                {
+                    name: 'Precio',
+                    selector: 'price',
+                    sortable: false,
+                    center: true,
+                    cell: (element: Product) => <React.Fragment>
+                        <Button
+                            onClick={() => this.removeCart(element)}
+                            variant="primary"
+                            icon="close"
+                            tooltip={true}
+                            labelTooltip="Remover del carrito"
+                        />
+                    </React.Fragment>
                 }
             ],
         }
     }
     componentDidMount(){
         this.load()
-        console.log('>>: cart > ', this.props.cart)
     }
 
+    removeCart = (element: Product) => {
+        let products = this.props.cart && [... this.props.cart] || []
+        const index = this.props.cart?.findIndex((_element: Product) => _element.id === element.id)
+        if(typeof index === 'number' && index !== -1){
+            products.splice(index, 1)
+        }
+        this.props.dispatch({
+            type: 'SET_CART',
+            payload: products
+        })
+    }
 
     load = async () => {
     }
